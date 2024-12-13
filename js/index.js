db8a7bb061059d7ab6f891c058ce87d96ae7ec26
Amany889900 <140924529+Amany889900@users.noreply.github.com>
2024-12-13 17:40:49 +0200

    done

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 var layout = document.getElementById("weatherInfo");
-
+var searchInput = document.getElementById("searchInput");
+var findBtn = document.getElementById("findBtn");
 var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
+var months = ['January','February','March','April','May','June','July','August','September','October','November','December']
 // {"location":{"name":"London","region":"City of London, Greater London","country":"United Kingdom","lat":51.5171,"lon":-0.1062,"tz_id":"Europe/London","localtime_epoch":1734023008,"localtime":"2024-12-12 17:03"},"current":{"last_updated_epoch":1734022800,"last_updated":"2024-12-12 17:00","temp_c":8.2,"temp_f":46.8,"is_day":0,"condition":{"text":"Light rain","icon":"//cdn.weatherapi.com/weather/64x64/night/296.png","code":1183},"wind_mph":5.4,"wind_kph":8.6,"wind_degree":48,"wind_dir":"NE","pressure_mb":1029.0,"pressure_in":30.39,"precip_mm":0.01,"precip_in":0.0,"humidity":93,"cloud":75,"feelslike_c":6.8,"feelslike_f":44.2,"windchill_c":6.7,"windchill_f":44.1,"heatindex_c":8.2,"heatindex_f":46.7,"dewpoint_c":7.1,"dewpoint_f":44.7,"vis_km":6.0,"vis_miles":3.0,"uv":0.0,"gust_mph":8.1,"gust_kph":13.0}}
 var query;
 async function display(query){
@@ -8,10 +10,13 @@ async function display(query){
 var Currentresponse = await fetch(`http://api.weatherapi.com/v1/current.json?key=bf0c5e874dc146058d0224947241112&q=${query}`);
 var Currentdata = await Currentresponse.json();
 
-var Forecastresponse = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=bf0c5e874dc146058d0224947241112&q=07112&days=2`);
+var Forecastresponse = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=bf0c5e874dc146058d0224947241112&q=${query}&days=2`);
 var Forecastdata = await Forecastresponse.json();
 var date = new Date(`${Currentdata.location.localtime}`);
 var weekDay =  days[date.getDay()];
+var secondDay = days[(date.getDay()+1)%7];
+var thirdDay = days[(date.getDay()+2)%7];
+var currentMonth = months[date.getMonth()];
 layout.innerHTML = `
 
 <div class="row container m-auto">
@@ -19,7 +24,7 @@ layout.innerHTML = `
           <div class="card-body bg-dark">
             <h5 class="card-title text-light border border-b-0 d-flex justify-content-between align-items-center p-2">
               <span class="">${weekDay}</span>
-              <span class="">${Currentdata.location.localtime}</span>
+              <span class="">${date.getDate()}${currentMonth}</span>
             </h5>
            <span class="text-light fs-5 d-inline-block p-3">${Currentdata.location.name}</span>
            <p class="display-3 text-light fw-bold">${Currentdata.current.temp_c} <sup>o</sup>C</p>
@@ -36,12 +41,12 @@ layout.innerHTML = `
 
         <div class="col col-md-6 col-lg-4 d-flex container gap-3">
           <div class="card-body bg-dark">
-            <h5 class="card-title text-light text-center border border-b-0 p-2">Dark card title</h5>
+            <h5 class="card-title text-light text-center border border-b-0 p-2">${secondDay}</h5>
             <div class="2ndDay text-center">
-              <div class="img-cont my-3"><img src="assets/113.webp" alt=""></div> <br>
-               <strong class="d-inline-block my-0 text-light fs-5">23.2<sup>o</sup>C</strong> <br>
-               <span class="d-inline-block text-light">23.2<sup>o</sup>C</span> <br><br>
-               <span class="d-inline-block mb-5 text-primary">Sunny</span>
+              <div class="img-cont my-3"><img src="${Forecastdata.forecast.forecastday[0].day.condition.icon}" alt=""></div> <br>
+               <strong class="d-inline-block my-0 text-light fs-5">${Forecastdata.forecast.forecastday[0].day.maxtemp_c}<sup>o</sup>C</strong> <br>
+               <span class="d-inline-block text-light">${Forecastdata.forecast.forecastday[0].day.mintemp_c}<sup>o</sup>C</span> <br><br>
+               <span class="d-inline-block mb-5 text-primary">${Forecastdata.forecast.forecastday[0].day.condition.text}</span>
             </div>
           </div>
           
@@ -49,12 +54,12 @@ layout.innerHTML = `
 
         <div class="col col-md-6 col-lg-4 d-flex container gap-3">
           <div class="card-body bg-dark">
-            <h5 class="card-title text-light text-center border border-b-0 p-2">Dark card title</h5>
-            <div class="3rdDay text-center">
-              <div class="img-cont my-3"><img src="assets/113.webp" alt=""></div> <br>
-               <strong class="d-inline-block my-0 text-light fs-5">23.2<sup>o</sup>C</strong> <br>
-               <span class="d-inline-block text-light">23.2<sup>o</sup>C</span> <br><br>
-               <span class="d-inline-block mb-5 text-primary">Sunny</span>
+            <h5 class="card-title text-light text-center border border-b-0 p-2">${thirdDay}</h5>
+            <div class="2ndDay text-center">
+              <div class="img-cont my-3"><img src="${Forecastdata.forecast.forecastday[1].day.condition.icon}" alt=""></div> <br>
+               <strong class="d-inline-block my-0 text-light fs-5">${Forecastdata.forecast.forecastday[1].day.maxtemp_c}<sup>o</sup>C</strong> <br>
+               <span class="d-inline-block text-light">${Forecastdata.forecast.forecastday[1].day.mintemp_c}<sup>o</sup>C</span> <br><br>
+               <span class="d-inline-block mb-5 text-primary">${Forecastdata.forecast.forecastday[1].day.condition.text}</span>
             </div>
           </div>
           
@@ -69,3 +74,29 @@ layout.innerHTML = `
 }
 
 display("Cairo");
+
+
+
+
+async function search(searchQuery){
+
+
+  var searchResponse = await fetch(` http://api.weatherapi.com/v1/search.json?key=bf0c5e874dc146058d0224947241112&q=${searchQuery}`);
+  var searchData = await searchResponse.json();
+
+  var city = searchData[0].name;
+
+  display(city);
+}
+
+searchInput.addEventListener("input", function(){
+  var searchQuery = searchInput.value;
+  search(searchQuery);
+}
+
+)
+
+findBtn.addEventListener("click", function(){
+  var searchQuery = searchInput.value;
+  display(searchQuery);
+})
